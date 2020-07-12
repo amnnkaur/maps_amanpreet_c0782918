eb0cef25553426145ff89196a3e1023e21614c40
Aman Kaur <amankaur2628@gmail.com>
2020-07-12 19:35:03 -0400

    DataParser, FetchURl and GetDirectionsData classes added

---
+++ app/src/main/java/com/lambton/maps_amanpreet_c0782918/IPassData.java
@@ -0,0 +1,9 @@
+package com.lambton.maps_amanpreet_c0782918;
+
+import android.location.Location;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public interface IPassData {
+    void destinationSelected(Location location, GoogleMap map);
+}
